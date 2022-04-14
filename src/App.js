3c8a7667b03b414c7467ba5faac026125a0a75ab
Martin <office@martinpagels.de>
2022-04-14 12:20:26 +0200

    get all studeten with fetch from data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/")
+      .then((response) => response.json())
+      .then((data) => {
+        setStudents(data);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <ul>
+        {students.map((student) => (
+          <li key={student._id}>
+            <h2>Name: {student.name}</h2>
+            <p>Age: {student.age}</p>
+            <p>Points: {student.points}</p>
+            <p>Happiness: {student.happiness}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
